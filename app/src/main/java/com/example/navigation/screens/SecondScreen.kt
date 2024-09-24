e35f9611fc78fda985673a4c405b48a537fbda87
pedro-donoso <pedro.donoso.r@usach.cl>
2024-09-23 22:32:01 -0300

    app funcionando

--- app/src/main/java/com/example/navigation/screens/SecondScreen.kt
+++ app/src/main/java/com/example/navigation/screens/SecondScreen.kt
@@ -1,7 +1,6 @@
 package com.example.navigation.screens
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -10,11 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
